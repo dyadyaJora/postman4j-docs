04ac753fea39a217ee6bc26b90ba8a96639d265d
dyadyaJora <idyadyajora@gmail.com>
2025-05-03 23:09:12 +0400

    fix: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,4 +7,8 @@ const withNextra = nextra({
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
   // ... Other Next.js config options
+  output: "export",
+  images: {
+    unoptimized: true,
+  }
 })
